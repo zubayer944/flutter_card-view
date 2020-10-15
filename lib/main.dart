7ba15c15f24de46e0ce04958d8be8114e0c304c1
Zubyer <zubayerhamim@gmail.com>
2020-10-15 13:24:08 +0600

    flutter grid view

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,49 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Card(
-            color: Colors.white,
-            elevation: 10,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(30))
-            ),
-            
-            shadowColor: Colors.red,
-            child: Container(
-              height: 200,
-              width: 200,
-            ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GridView.count(
+            mainAxisSpacing: 5,
+            crossAxisSpacing: 5,
+            crossAxisCount: 2,
+            children: [
+              Container(
+                height: 200,
+                width: 150,
+                color: Colors.red,
+              ),
+              Container(
+                height: 200,
+                width: 150,
+                color: Colors.red,
+              ),
+              Container(
+                height: 200,
+                width: 150,
+                color: Colors.red,
+              ),
+              Container(
+                height: 200,
+                width: 150,
+                color: Colors.red,
+              ),
+              Container(
+                height: 200,
+                width: 150,
+                color: Colors.red,
+              )
+            ],
           ),
         ),
       ),
